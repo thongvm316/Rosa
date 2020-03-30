12405d751a7bcd49178c37427a7d337e3904937f
Minh Thong <vominhthong1603@gmail.com>
2020-03-30 20:35:40 +0700

    20200330

--- js/main.js
+++ js/main.js
@@ -11,3 +11,34 @@ console.log({menuTogger, body});
 menuTogger.addEventListener('click', function () { 
     body.classList.toggle('open');
 })
+
+/* Scroll */
+window.sr = ScrollReveal();
+
+sr.reveal('.animate-left',{
+  origin: 'left',
+  duration: 1000,
+  distance: '25rem',
+  delay: 300
+});
+
+sr.reveal('.animate-right',{
+  origin: 'right',
+  duration: 1000,
+  distance: '25rem',
+  delay: 600
+});
+
+sr.reveal('.animate-top',{
+  origin: 'top',
+  duration: 1000,
+  distance: '25rem',
+  delay: 600
+});
+
+sr.reveal('.animate-bottom',{
+  origin: 'bottom',
+  duration: 1000,
+  distance: '25rem',
+  delay: 600
+});
